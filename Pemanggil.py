57e0804670962f7ace26ad9028622c50bf6d26d8
chelseasr321 <chelseasr321@gmail.com>
2026-06-12 20:57:59 +0700

    Update Pemanggil.py

--- Pemanggil.py
+++ Pemanggil.py
@@ -1,1 +1,15 @@
+import chelsea047
 
+matriks = [
+    [1, 2, 3],
+    [0, 4, 5],
+    [1, 0, 6]
+]
+
+hasil = chelsea047.determinan3x3(matriks)
+
+print("Matriks:")
+for baris in matriks:
+    print(baris)
+
+print("\nDeterminan =", hasil)
